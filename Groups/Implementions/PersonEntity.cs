cda25cfc0b79000d7541332d7c7327d2ce611d65
Albinosos <laklaym698@gmail.com>
2024-03-28 21:07:31 +0200

    two more classes added

--- Groups/Implementions/PersonEntity.cs
+++ Groups/Implementions/PersonEntity.cs
@@ -2,5 +2,12 @@ namespace Groups.Implementions;
 
 public class PersonEntity
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
 
+    public PersonEntity(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
 }
