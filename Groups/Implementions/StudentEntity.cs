e5b245b180d6586cb88cf5186253b57b074493d7
Albinosos <laklaym698@gmail.com>
2024-03-28 21:15:15 +0200

    Merge step3

--- Groups/Implementions/StudentEntity.cs
+++ Groups/Implementions/StudentEntity.cs
@@ -2,5 +2,10 @@ namespace Groups.Implementions;
 
 public class StudentEntity : PersonEntity
 {
+    public string School { get; set; }
 
+    public StudentEntity(int id, string name, string school) : base(id, name)
+    {
+        School = school ?? throw new ArgumentNullException(nameof(school));
+    }
 }
--- Groups/Implementions/TeacherEntity.cs
+++ Groups/Implementions/TeacherEntity.cs
@@ -4,5 +4,10 @@ namespace Students.Implementations;
 
 public class TeacherEntity : PersonEntity
 {
+    public string Department { get; set; }
 
+    public TeacherEntity(int id, string name, string department) : base(id, name)
+    {
+        Department = department ?? throw new ArgumentNullException(nameof(department));
+    }
 }
